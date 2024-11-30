ad5e02f13525b18da2dd1f321b482cbcd4599216
ahmadparvej <ahmadparvej58@gmail.com>
2024-11-30 21:44:52 +0530

    add protected routes

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,6 +1,14 @@
 
 import { Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import { useAuthStore } from '../store/store';
 export const AuthLayout = () => {
+
+  const { user } = useAuthStore();
+  if(user !== null){
+    return <Navigate to="/" replace={true}/>
+  }
+
   return (
     <>
       <Outlet/>
--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuthStore } from "../store/store";
 
 export const DashboardLayout = () => {
+
+  const { user } = useAuthStore();
+  if(user == null){
+    return <Navigate to="/auth/login" replace={true}/>
+  }
+
   return (
     <>
       <Outlet/>
