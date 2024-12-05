247063708f3a6992bac58c724947895f6b32dead
ahmadparvej <ahmadparvej58@gmail.com>
2024-12-05 20:51:20 +0530

    add users table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,17 +1,68 @@
-import { Breadcrumb } from "antd"
-import { RightOutlined } from "@ant-design/icons"
-import { Link } from "react-router-dom"
+import { Breadcrumb, Space, Table, TableProps } from "antd";
+import { RightOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getUsers } from "../../http/api";
+import { User } from "../../types";
+
+const columns: TableProps<User>["columns"] = [
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (text, record) => {
+      return (
+        <Link to={`/users/${record.id}`}>
+          {text} {record.lastName}
+        </Link>
+      );
+    },
+  },
+  {
+    title: "email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+  {
+    title: "Action",
+    key: "action",
+    render: (_, record) => (
+      <span>
+        <Link to={`/users/${record.id}/edit`}>Edit</Link>
+      </span>
+    ),
+  },
+];
+
+const breadcrumbItems = [
+  { title: <Link to={"/"}>Dashboard</Link> },
+  { title: "Users" },
+];
 
 export const UsersPage = () => {
-
-  const breadcrumbItems = [
-    { title: <Link to={'/'}>Dashboard</Link>},
-    { title: 'Users' }
-  ]
+  const {
+    data: users,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: getUsers,
+  });
 
   return (
     <>
       <Breadcrumb separator={<RightOutlined />} items={breadcrumbItems} />
+      <Space direction="vertical" size={"large"} style={{ width: "100%" }}>
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>{error.message}</div>}
+        {users && <Table columns={columns} dataSource={users} />}
+      </Space>
     </>
-  )
-}
+  );
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,11 @@ export type Credentials = {
     email: string;
     password: string;
 };
+
+export type User = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: string;
+};
